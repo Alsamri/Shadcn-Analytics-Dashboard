b0bf43446690a8f2076a7b1afbaa8e6b237bd644
Alsamri <azozid@yahoo.com>
2025-06-17 18:17:45 +0100

    avatar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { BadgePlus } from "lucide-react";
 
 export default function Home() {
   return (
     <div className="h-screen flex items-center justify-center">
-      <Button variant={"destructive"}>click me</Button>
+      <Button variant={"destructive"}>
+        <BadgePlus />
+        click me
+      </Button>
     </div>
   );
 }
