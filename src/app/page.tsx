161715461ae6fa2094a1419bf32979be39cc127f
Alsamri <azozid@yahoo.com>
2025-06-30 15:36:14 +0100

    basig bar charts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import AppBarChart from "@/components/AppBarChart";
 import { Button } from "@/components/ui/button";
 import { BadgePlus } from "lucide-react";
 
 export default function Home() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
-      <div className="bg-primary-foreground p-4 rounded-lg">test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <AppBarChart />
+      </div>
       <div className="bg-primary-foreground p-4 rounded-lg">test</div>
       <div className="bg-primary-foreground p-4 rounded-lg">test</div>
       <div className="bg-primary-foreground p-4 rounded-lg">test</div>
