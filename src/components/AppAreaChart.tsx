2f79b7428c4d6854c5fb4906f3dd2bd4df006512
Alsamri <azozid@yahoo.com>
2025-07-07 15:30:10 +0100

    area chart added with gradiant

--- src/components/AppAreaChart.tsx
+++ src/components/AppAreaChart.tsx
@@ -9,6 +9,7 @@ import {
 } from "@/components/ui/chart";
 
 import {
+  Area,
   AreaChart,
   Bar,
   BarChart,
@@ -52,9 +53,48 @@ const AppAreaChart = () => {
           />
           <YAxis tickLine={false} tickMargin={10} axisLine={false} />
           <ChartTooltip content={<ChartTooltipContent />} />
-
-          <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
-          <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+          <defs>
+            <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
+              <stop
+                offset="5%"
+                stopColor="var(--color-desktop)"
+                stopOpacity={0.8}
+              />
+              <stop
+                offset="95%"
+                stopColor="var(--color-desktop)"
+                stopOpacity={0.1}
+              />
+            </linearGradient>
+            <linearGradient id="fillMobile" x1="0" y1="0" x2="0" y2="1">
+              <stop
+                offset="5%"
+                stopColor="var(--color-mobile)"
+                stopOpacity={0.8}
+              />
+              <stop
+                offset="95%"
+                stopColor="var(--color-mobile)"
+                stopOpacity={0.1}
+              />
+            </linearGradient>
+          </defs>
+          <Area
+            dataKey="mobile"
+            type="natural"
+            fill="url(#fillMobile)"
+            fillOpacity={0.4}
+            stroke="var(--color-mobile)"
+            stackId="a"
+          />
+          <Area
+            dataKey="desktop"
+            type="natural"
+            fill="url(#fillDesktop)"
+            fillOpacity={0.4}
+            stroke="var(--color-desktop)"
+            stackId="a"
+          />
         </AreaChart>
       </ChartContainer>
     </div>
